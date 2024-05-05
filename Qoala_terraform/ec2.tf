e5ab3549ffff556125e47d7da8ec9fc1c84c11e7
g0707 <gauravtr28@gmail.com>
2024-05-05 18:28:38 +0530

    New changes

--- Qoala_terraform/ec2.tf
+++ Qoala_terraform/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "private_instance" {
   ami           = "ami-07caf09b362be10b8"
   instance_type = "t2.micro"
+  subnet_id = aws_subnet.private_subnets.id
 
   tags = {
     Name = "Private Subnet Instance"
